5f2bddf540a3c30273ae53dec28c89f80c0230c8
Roman <student.rv86@gmail.com>
2019-03-19 17:32:23 +0200

    edited Primitive

--- src/com/company/PrimitiveShape.java
+++ src/com/company/PrimitiveShape.java
@@ -1,6 +1,18 @@
 package com.company;
 
 public class PrimitiveShape {
+	
+	public static class PrimitiveBoard {
+		
+		private List<PrimitiveShape> list;
+		
+		public PrimitiveBoard {
+		}
+		
+		public void add(PrimitiveShape primitiveShape) {
+			list.add(primitiveShape);
+		}
+	}
     
 	private Shape.ShapeType shapeType;
 	private double x;
@@ -37,13 +49,13 @@ public class PrimitiveShape {
 			output.size = tmpShape.getSize();
 		}
 		if(shape instanceof Ball) {
-			
+			output.shapeType = Shape.ShapeType.BALL;
 		}
 		if(shape instanceof Square) {
-			
+			output.shapeType = Shape.ShapeType.SQUARE;
 		}
 		if(shape instanceof Triangle) {
-			
+			output.shapeType = Shape.ShapeType.TRIANGLE;
 		}
 		if(shape instanceof Group) {
 			
@@ -53,7 +65,25 @@ public class PrimitiveShape {
 	}
 	
 	public static Shape primitiveToShape(PrimitiveShape primitive) {
-		
-	}
-	
+		double x = primitive.getX();
+		double y = primitive.getY();
+		double size = primitive.getSize;
+		Shape.ShapeType shapeType = primitive.getShapeType();
+		switch(shapeType) {
+			case BALL:
+				board.addBall(x, y, size);
+				break;
+			case SQUARE:
+				board.addSquare(x, y, size);
+				break;
+			case TRIANGLE:
+				board.addTriangle(x, y, size);
+				break;
+			case GROUP:
+			
+				break;
+			default:
+				break;		
+		}
+	}	
 }
